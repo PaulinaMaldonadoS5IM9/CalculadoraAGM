0217564a4f594eb613f99a33d323cd206405fd46
AlexanderSanchezGarcia <sanchez.garcia.miguel.alexandeer.6@gmail.com>
2021-11-26 03:58:02 -0600

    Funcion Radianes

--- app/src/main/java/com/agm/corporation/calculadoraagm/RadianesActivity.java
+++ app/src/main/java/com/agm/corporation/calculadoraagm/RadianesActivity.java
@@ -1,14 +1,62 @@
 package com.agm.corporation.calculadoraagm;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+public class RadianesActivity extends AppCompatActivity implements View.OnFocusChangeListener {
 
-public class RadianesActivity extends AppCompatActivity {
+    private EditText editTextXRad;
+    private EditText editTextYRad;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_radianes);
+        editTextXRad = findViewById(R.id.editTextNumberXRad);
+        editTextYRad = findViewById(R.id.editTextNumberYRad);
+        editTextXRad.setOnFocusChangeListener(this);
+        editTextYRad.setOnFocusChangeListener(this);
+    }
+
+    @Override
+    public void onFocusChange(View view, boolean hasFocus) {
+        if(!hasFocus) {
+            realizaOperacion();
+        }
+    }
+
+    private void realizaOperacion() {
+
+        if(editTextXRad.getText().toString() != null) {
+            if(isDouble(editTextXRad.getText().toString())) {
+                double deg = Math.toRadians(getDouble(editTextXRad.getText().toString()));
+                editTextYRad.setText(String.valueOf(deg));
+            }
+        }
+
+    }
+
+    private double getDouble(String entero) {
+        try {
+            return Double.parseDouble(entero);
+        } catch(NumberFormatException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+    private boolean isDouble(String entero) {
+        try {
+            if(entero == null || entero.length() == 0) {
+                return false;
+            }
+            Double.parseDouble(entero);
+            return true;
+        } catch(NumberFormatException e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 }
